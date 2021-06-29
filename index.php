4241c6ca5d86e434feb0c217b04b2e350f8324d0
dumi994 <dumi.9944@gmail.com>
2021-06-29 16:16:53 +0200

    creato classe premium user e credit card

--- index.php
+++ index.php
@@ -5,13 +5,16 @@ class Prodotti {
     public $price;
     public $qty;
     public $weight;
-    function __construct(string $name, string $model, int $price, int $qty, string $weight)
+    function __construct(string $name, string $model, $price, int $qty, string $weight)
     {
         $this->name = $name;
         $this->model = $model;
         $this->price = $price;
         $this->qty = $qty;
         $this->weight = $weight;
+    }
+    function name(){
+        return $this->name;
     }
 }
 $prodotti = [
@@ -24,38 +27,91 @@ $prodotti = [
 var_dump($prodotti);
 
 
-class Utenti extends Prodotti {
-    
-    function __construct(string $name, string $model,  int $price, int $qty, bool $premium)
-       {
-           parent::__construct($name, $model, $price, $qty, $premium);
-           $this->premium = $premium;
-           if ($premium = true){
-               $this->price -= $price * 10 / 100;
-           }else{
-               $this->price = $price;
-           }
+class User {
+    protected $name;
+    protected $lastname;
+    protected $email;
+    protected $username;
+    protected $password;
+    public function __construct (string $name, string $lastname, string $email, string $username, string $password){
+        $this->name = $name;
+        $this->lastname = $lastname;
+        $this->email = $email;
+        $this->username = $username;
+        $this->password = $password;
     }
-    public $name;
-    public $premium;
-    
-}
-$users = [
-    $gino = new Utenti ('Gino', 'X', 50.00, 2, true),
-    $mario = new Utenti ('Mario', 'Y', 200, 2, false),
-];
-var_dump($users);
 
-
-/* class Utenti extends Prodotti {
-    function __construct(string $name, string $model, int $price, int $qty, string $weight, $price)
+    public function insertCreditCard($cc)
     {
-        parent::__construct(string $name, string $model, int $price, int $qty, string $weight),
-        $this->price = $price;
+        return $cc;
     }
-}   
- */
+}
+class PremiumUser extends User{
+    protected $isPremium = true;
+    protected $discount;
+    public function discount()
+    {
+       $this->discount = 20;
+       return $this->discount;
+    }
+    public function isPremium()
+    {
+        return $this->premium;
+    }
+}
+class CreditCard{
+    protected $number;
+    protected $expDate;
+    protected $ccv;
+    public $bool;
 
+    public function __construct($number, $expDate, $ccv,bool $bool)
+    {
+        $this->number = $number;
+        $this->expDate = $expDate;
+        $this->ccv = $ccv;
+        $this->bool = $bool;
+        if ($this->bool == true){
+            return 'pagamento effettuato';
+        }   
+    }
+}
+$card = new CreditCard(1234567890, '12/05/22', 123, true);
 
+$users = [
+    
+    new User('Federico', 'Pieri', 'email@test.com', 'fedearn', '1234'),
+    new User('Carlo', 'Burzi', 'emai123l@test.com', 'Carloburz', '1234'),
+    new User('Giacomo', 'Pena', '123email@test.com', 'giacomopena', '1234'),
+    new PremiumUser('Maura', 'Stacchini', 'e223mail@test.com', 'maurastacch', '1234')
+
+];
+/*   */
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP 2</title>
+</head>
+<body>
+    <?php 
+    foreach ($prodotti as $product) : ?>
+    <h1><?= $product->name;?></h1>
+    <p><?= $product->model;?></p>
+    <p><?= $product->price;?>$</p>
+
+    <?php endforeach; ?>
+
+   <?php 
+    foreach ($users as $user) :?>
+    
+    <h2>Ciao<?= $user[0]->name;?></h2>
+    <p><?=  $user->insertCreditCard($card)?></p>
+    <p>Hai diritto ad un <?= $user[3]->discount();?> di sconto</p>
+   <?php endforeach; ?>
+</body>
+</html>
